7f176e0504641ea73a6141c5d858c253f791e99d
bam-mcs <bbehinaein@MacquarieCloudServices.com>
2026-07-22 10:37:45 +1000

    Committing 2 items from workspace ff6a810c-a472-4e49-9583-835e0c5cb4fd

---
+++ fabric/testing.Notebook/notebook-content.py
@@ -0,0 +1,53 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {}
+# META }
+
+# CELL ********************
+
+import notebookutils
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+vl = notebookutils.variableLibrary.getLibrary("deployment_vars")
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+vl.getVariable("test_var")
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
